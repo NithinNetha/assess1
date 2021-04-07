bef96469dee803c99ea56fe522d49988be0ee4e0
Nithin Palarapu <nithin6425@gmail.com>
2021-04-07 23:15:33 +0530

    Tags added in Home.java

--- Home.java
+++ Home.java
@@ -3,37 +3,36 @@ import java.io.*;
 public class Home {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		int ch;
 		String filecreate;
 		BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
 		String line="Welcome to LockedMe.com"+
 					"\nDeveloped by Nithin Palarapu";
-		System.out.println(line);
+		System.out.println(line); //print welcome note
 		do {
-			System.out.println("Select option from following list");
+			System.out.println("Select option from following list"); //Asking input for options
 			System.out.println("\t1.Add file\n\t2.Delete file\n\t3.Search file\n\t4.Display files\n\t5.Exit");
-			ch=Integer.parseInt(reader.readLine());
+			ch=Integer.parseInt(reader.readLine()); //Takes input for option.
 			if(ch>5)
-				System.out.println("***Entered output is invalid***\n");
-			switch(ch) {
-			case 1:
-				System.out.print("Enter file name to create: ");
-				filecreate=reader.readLine();
-				AddFile af= new AddFile(filecreate);
+				System.out.println("***Entered output is invalid***\n"); //prints when wrong option selected.
+			switch(ch) { //start switch case for entered options
+			case 1: //Add files
+				System.out.print("Enter file name to create: "); //ask for file name.
+				filecreate=reader.readLine(); //reads file name.
+				AddFile af= new AddFile(filecreate); //AddFile() method is called.
 				break;
-			case 2:
+			case 2: //Delete file
 				//Deletefile();
 				break;
-			case 3:
+			case 3: //Search desired file
 				//Search();
 				break;
-			case 4:
+			case 4: //Display files in alphabetical order.
 				//Display();
 				break;
 			}
-		}while(ch!=5);
-		System.out.println("\nThanks for using this applicaion");
+		}while(ch!=5); //do..while() loop exits if option selected is 5.
+		System.out.println("\nThanks for using this applicaion"); //Thanks greet.
 	}
 
 }
